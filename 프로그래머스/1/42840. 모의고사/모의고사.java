ab0879cc4f0d13f2e4f24bc762fc29889e5eb38d
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-04-05 17:09:25 +0900

    [level 1] Title: 모의고사, Time: 2.94 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,39 +1,20 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int[] arr) {
-        Map<Integer,Integer> hashMap = new LinkedHashMap<>();
-        ArrayList<Integer> person1 = new ArrayList<>(Arrays.asList(1,2,3,4,5));
-        ArrayList<Integer> person2 = new ArrayList<>(Arrays.asList(2,1,2,3,2,4,2,5));
-        ArrayList<Integer> person3 = new ArrayList<>(Arrays.asList(3,3,1,1,2,2,4,4,5,5));
-        int person1Cnt = 0;
-        int person2Cnt = 0;
-        int person3Cnt = 0;
-        for(int i = 0; i < arr.length; i++){
-            if(person1.get(i % person1.size()).equals(arr[i])){  // % 로 순환
-                person1Cnt++;
-            }
-            if(person2.get(i % person2.size()).equals(arr[i])){
-                person2Cnt++;
-            }
-            if(person3.get(i % person3.size()).equals(arr[i])){
-                person3Cnt++;
-            }
+    public int[] solution(int[] answer) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[3];
+        for(int i=0; i<answer.length; i++) {
+            if(answer[i] == a[i%a.length]) {score[0]++;}
+            if(answer[i] == b[i%b.length]) {score[1]++;}
+            if(answer[i] == c[i%c.length]) {score[2]++;}
         }
-        hashMap.put(1,person1Cnt);
-        hashMap.put(2,person2Cnt);
-        hashMap.put(3,person3Cnt);
-
-        // 최댓값 찾기
-        int maxVal = Collections.max(hashMap.values());
-
-        // 최댓값인 key만 리스트에 담기
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
         ArrayList<Integer> list = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> e : hashMap.entrySet()) {
-            if (e.getValue() == maxVal) {
-                list.add(e.getKey());
-            }
-        }
-
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        if(maxScore == score[0]) {list.add(1);}
+        if(maxScore == score[1]) {list.add(2);}
+        if(maxScore == score[2]) {list.add(3);}
+        return list.stream().mapToInt(i->i.intValue()).toArray();
     }
 }
