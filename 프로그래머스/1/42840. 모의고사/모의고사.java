10ff3fbef60b0ff4884224dce075a9a0bc181ef7
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-04-06 12:03:46 +0900

    [level 1] Title: 모의고사, Time: 0.91 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int[] arr) {
+    public int[] solution(int[] answers) {
         int[] person1 = {1,2,3,4,5};
         int[] person2 = {2,1,2,3,2,4,2,5};
         int[] person3 = {3,3,1,1,2,2,4,4,5,5};
@@ -8,23 +8,29 @@ class Solution {
         int person2Cnt = 0;
         int person3Cnt = 0;
 
-        for(int i = 0; i < arr.length; i++){
-            if(person1[i % person1.length] == arr[i]) person1Cnt++;
-            if(person2[i % person2.length] == arr[i]) person2Cnt++;
-            if(person3[i % person3.length] == arr[i]) person3Cnt++;
+        for(int i = 0; i < answers.length; i++){
+            if(answers[i] == person1[i % person1.length]) person1Cnt++;
+            if(answers[i] == person2[i % person2.length]) person2Cnt++;
+            if(answers[i] == person3[i % person3.length]) person3Cnt++;
         }
 
-        int maxVal = Math.max(person1Cnt, Math.max(person2Cnt, person3Cnt));
+        int maxCnt = Math.max(person1Cnt, Math.max(person2Cnt,person3Cnt));
+        ArrayList<Integer> answerList = new ArrayList<>();
+        answerList.add(person1Cnt);
+        answerList.add(person2Cnt);
+        answerList.add(person3Cnt);
 
-        ArrayList<Integer> list = new ArrayList<>();
-        if(person1Cnt == maxVal) list.add(1);
-        if(person2Cnt == maxVal) list.add(2);
-        if(person3Cnt == maxVal) list.add(3);
 
-        int[] answer = new int[list.size()];
         int cnt = 0;
-        for(int num : list)
-            answer[cnt++] = num;
+        for(int i = 0; i < answerList.size(); i++){
+            if(answerList.get(i) == maxCnt) cnt++;
+        }
+        int[] answer = new int[cnt];
+        int answerCnt = 0;
+        for(int i = 0; i < answerList.size(); i++){
+            if(answerList.get(i) == maxCnt) answer[answerCnt++] = (i+1);
+        }
+
         return answer;
     }
 }
