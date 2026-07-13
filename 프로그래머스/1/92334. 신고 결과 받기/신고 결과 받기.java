1d211eae3414ca1cf5c2085a9e4a98e5e8242662
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-14 00:07:38 +0900

    [level 1] Title: 신고 결과 받기, Time: 124.26 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,44 +1,40 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        HashMap<String,Integer> idMap = new HashMap<>();
-        HashMap<String,Integer> countMap = new HashMap<>();
-        HashMap<String, HashSet<String>> veriMap = new HashMap<>();
         int[] answer = new int[id_list.length];
-        for (String id : id_list) {
-            idMap.put(id, idMap.getOrDefault(id,0));
-            countMap.put(id, countMap.getOrDefault(id,0));
+
+        HashMap<String, HashSet<String>> hashMap = new HashMap<>();
+        HashMap<String, Integer> countMap = new HashMap<>();
+        for(String reportStr : report) {
+            String[] reportArr = reportStr.split(" ");
+            String from = reportArr[0];
+            String to = reportArr[1];
+            HashSet<String> hashSet;
+            if(hashMap.get(to) != null) {
+                hashSet = hashMap.get(to);
+            }else {
+                hashSet = new HashSet<>();
+            }
+            hashSet.add(from);
+            hashMap.put(to, hashSet);
         }
 
-        for(String id : report) {
-
-            String[] reportSplit = id.split(" ");
-            String from = reportSplit[0];
-            String to = reportSplit[1];
-            HashSet<String> set;
-            if(veriMap.get(to) != null) {
-                set = veriMap.get(to);
-            } else {
-                set = new HashSet<>();
-            }
-            if(veriMap.get(to) == null || !veriMap.get(to).contains(from)){
-                idMap.put(to, idMap.get(to) + 1);
-            }
-            set.add(from);
-            veriMap.put(to, set);
-        }
-
-        for(String from : idMap.keySet()) {
-            for(String to : veriMap.keySet()) {
-                if(veriMap.get(to).contains(from) && idMap.get(to) >= k) {
-                    countMap.put(from, countMap.getOrDefault(from, 0) +1);
+        for(String reportStr : hashMap.keySet()) {
+            if(hashMap.get(reportStr).size() >= k) {
+                for(String from : hashMap.get(reportStr)) {
+                    countMap.put(from, countMap.getOrDefault(from,0) +1);
                 }
             }
         }
-        for(int i = 0; i < id_list.length; i++){
-            String name = id_list[i]; // 순서대로 이름을 꺼낸다
-            answer[i] = countMap.getOrDefault(name, 0);
+
+        for(int i = 0; i < id_list.length; i++) {
+            if(countMap.get(id_list[i]) != null) {
+                answer[i] = countMap.get(id_list[i]);
+            } else {
+                answer[i] = 0;
+            }
         }
         return answer;
     }
