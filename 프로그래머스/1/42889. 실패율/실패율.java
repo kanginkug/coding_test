748bf46ed5b7a09dfc0cb09ef3d67e3cedf087b7
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-14 01:00:47 +0900

    [level 1] Title: 실패율, Time: 20.12 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,50 +1,51 @@
 import java.util.*;
 class Solution {
-    // 1. 스테이지 번호와 실패율을 함께 묶을 클래스 생성
-    class Stage implements Comparable<Stage> {
-        int id;          // 스테이지 번호
-        double rate;     // 실패율
+    class Stage {
+        int id;
+        double rate;
 
         public Stage(int id, double rate) {
             this.id = id;
             this.rate = rate;
         }
-
-        // 2. 정렬 기준 정의 (자바 정렬 문법)
-        @Override
-        public int compareTo(Stage o) {
-            if (this.rate == o.rate) {
-                return Integer.compare(this.id, o.id); // 실패율이 같으면 스테이지 번호 오름차순
-            }
-            return Double.compare(o.rate, this.rate); // 실패율 기준 내림차순 (o와 this의 위치를 바꾸면 내림차순이 됨)
-        }
     }
-
     public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
-        int stageLength = stages.length;
-        List<Stage> stageList = new ArrayList<>();
-        stages = Arrays.stream(stages).sorted().toArray();
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-
+        double personTotal = stages.length;
+        ArrayList<Integer> stageList = new ArrayList();
+        HashMap<Integer, Integer> stageMap = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Stage> list = new ArrayList<>();
         for(int stage : stages) {
-            hashMap.put(stage, hashMap.getOrDefault(stage, 0) +1);
+            map.put(stage, map.getOrDefault(stage,0) +1);
         }
-
-        for(int n =1; n <= N; n++) {
-            int stageNum = hashMap.getOrDefault(n, 0);
-            double rate = 0.0;
-            if(stageLength > 0) {
-                rate = (double) stageNum / stageLength;
+        for(int i = 1; i <= N; i++) {
+            if(map.containsKey(i)) {
+                stageMap.put(i, map.get(i));
+            } else if(!map.containsKey(i)) {
+                stageMap.put(i, 0);
             }
-            Stage stage = new Stage(n, rate);
-            stageList.add(stage);
-            stageLength = stageLength - stageNum;
         }
 
-        Collections.sort(stageList);
-        for(int i = 0; i < stageList.size(); i++) {
-            answer[i] = stageList.get(i).id;
+        for(int stage : stageMap.keySet()) {
+            int stagePerson = stageMap.get(stage);
+            if(personTotal != 0.0) {
+                Stage stage1 = new Stage(stage, stagePerson / personTotal);
+                personTotal = personTotal - stagePerson;
+                list.add(stage1);
+            } else {
+                Stage stage1 = new Stage(stage, 0);
+                list.add(stage1);
+            }
+        }
+
+        list.sort((a,b) -> {
+            if (a.rate == b.rate) return Integer.compare(a.id, b.id);
+            return Double.compare(b.rate, a.rate);
+        });
+
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i).id;
         }
         return answer;
     }
