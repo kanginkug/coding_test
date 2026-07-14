2dc09cba50dcf0fcc3110e098f1e346151b3f725
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-14 10:32:52 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 16.80 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,14 +3,12 @@ import java.util.ArrayList;
 public class Solution {
     public int[] solution(int []arr) {
         ArrayList<Integer> list = new ArrayList();
-        for(int i = 1; i < arr.length; i++) {
-            int prefix = arr[i-1];
-            if(i == 1) {
-                list.add(prefix);
-            }
+        int prefix = 10;
+        for(int i = 0; i < arr.length; i++) {
             if(arr[i] != prefix) {
                 list.add(arr[i]);
             }
+            prefix = arr[i];
         }
 
         int[] answer = new int[list.size()];
