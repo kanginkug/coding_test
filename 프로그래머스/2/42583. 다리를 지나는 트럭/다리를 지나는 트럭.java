810b9ab84805307d5cd9aeb36139b7efc0dc5701
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-21 21:00:38 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 16.19 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,30 +3,28 @@ import java.util.ArrayList;
 import java.util.Queue;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        ArrayList<Integer> arrList = new ArrayList<>();
-        for(int truck : truck_weights) {
-            arrList.add(truck);
-        }
         Queue<Integer> bridgeQueue = new ArrayDeque<>();
-        for(int i = 0; i < bridge_length; i++) {
+        for(int i = 0; i < bridge_length; i++){
             bridgeQueue.offer(0);
         }
-        int answer = 0;
+        ArrayList<Integer> truckList = new ArrayList<>();
+        for(int a : truck_weights) {
+            truckList.add(a);
+        }
+
         int totalWeight = 0;
-        while(!bridgeQueue.isEmpty()) {
+        int answer = 0;
+        while(!bridgeQueue.isEmpty()){
             answer++;
             int peek = bridgeQueue.poll();
             totalWeight = totalWeight - peek;
-            for(int i = 0; i < truck_weights.length; i++) {
-                if(truck_weights[i] > 0){
-                    if(totalWeight + truck_weights[i] > weight) {
-                        bridgeQueue.offer(0);
-                    } else {
-                        totalWeight = totalWeight + truck_weights[i];
-                        bridgeQueue.offer(truck_weights[i]);
-                        truck_weights[i]=0;
-                    }
-                    break;
+            if(!truckList.isEmpty()) {
+                if(truckList.get(0) + totalWeight > weight) {
+                    bridgeQueue.offer(0);
+                } else {
+                    bridgeQueue.offer(truckList.get(0));
+                    totalWeight = totalWeight + truckList.get(0);
+                    truckList.remove(0);
                 }
             }
         }
