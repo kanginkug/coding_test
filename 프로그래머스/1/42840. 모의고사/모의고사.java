0dd0ed5cbd717f600b734e161d555472fe52b16a
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-21 10:56:09 +0900

    [level 1] Title: 모의고사, Time: 2.20 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,36 +1,58 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 class Solution {
+    class Student {
+        int cnt;
+        int name;
+
+        public Student(int cnt, int name) {
+            this.cnt = cnt;
+            this.name = name;
+        }
+    }
     public int[] solution(int[] answers) {
-        int[] person1 = {1,2,3,4,5};
-        int[] person2 = {2,1,2,3,2,4,2,5};
-        int[] person3 = {3,3,1,1,2,2,4,4,5,5};
-        int person1Cnt = 0;
-        int person2Cnt = 0;
-        int person3Cnt = 0;
+        ArrayList<Student> answerList = new ArrayList<>();
+        int[] one = {1, 2, 3, 4, 5};
+        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int oneAns = 0;
+        int twoAns = 0;
+        int threeAns = 0;
 
-        for(int i = 0; i < answers.length; i++){
-            if(answers[i] == person1[i % person1.length]) person1Cnt++;
-            if(answers[i] == person2[i % person2.length]) person2Cnt++;
-            if(answers[i] == person3[i % person3.length]) person3Cnt++;
+        for(int i = 0; i < answers.length; i++) {
+            if (answers[i] == one[i % 5]) {
+                oneAns++;
+            } if (answers[i] == two[i % 8]) {
+                twoAns++;
+            } if (answers[i] == three[i % 10]) {
+                threeAns++;
+            }
         }
 
-        int maxCnt = Math.max(person1Cnt, Math.max(person2Cnt,person3Cnt));
-        ArrayList<Integer> answerList = new ArrayList<>();
-        answerList.add(person1Cnt);
-        answerList.add(person2Cnt);
-        answerList.add(person3Cnt);
+        Student oneStu = new Student(oneAns, 1);
+        Student twoStu = new Student(twoAns, 2);
+        Student threeStu = new Student(threeAns, 3);
+        answerList.add(oneStu);
+        answerList.add(twoStu);
+        answerList.add(threeStu);
+        answerList.sort((a,b) -> Integer.compare(b.cnt, a.cnt));
 
-
-        int cnt = 0;
-        for(int i = 0; i < answerList.size(); i++){
-            if(answerList.get(i) == maxCnt) cnt++;
+        ArrayList<Integer> list = new ArrayList<>();
+        if(answerList.get(0).cnt > answerList.get(1).cnt) {
+            list.add(answerList.get(0).name);
+        } else if(answerList.get(0).cnt == answerList.get(1).cnt && answerList.get(1).cnt > answerList.get(2).cnt) {
+            list.add(answerList.get(0).name);
+            list.add(answerList.get(1).name);
+        } else if(answerList.get(0).cnt == answerList.get(1).cnt && answerList.get(1).cnt == answerList.get(2).cnt) {
+            list.add(answerList.get(0).name);
+            list.add(answerList.get(1).name);
+            list.add(answerList.get(2).name);
         }
-        int[] answer = new int[cnt];
-        int answerCnt = 0;
-        for(int i = 0; i < answerList.size(); i++){
-            if(answerList.get(i) == maxCnt) answer[answerCnt++] = (i+1);
-        }
 
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 }
