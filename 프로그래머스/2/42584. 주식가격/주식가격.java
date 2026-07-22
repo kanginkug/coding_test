bcbdfe09a4aaff98af44ddd026f83e432999e399
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-22 15:56:09 +0900

    [level 2] Title: 주식가격, Time: 13.84 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,33 +1,18 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        int max = 0;
-        for(int price : prices) {
-            if(price > max) {
-                max = price;
-            }
-            queue.offer(price);
-        }
         int[] answer = new int[prices.length];
-        if(max == 0) {
-            for(int i = 0; i < prices.length; i++) {
-                answer[i] = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0; i < prices.length; i++) {
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int peek = stack.pop();
+                answer[peek] = i - peek;
             }
-            return answer;
+            stack.push(i);
         }
-        int inx = 0;
-        while (!queue.isEmpty()) {
-            int poll = queue.poll();
-            int cnt = -1;
-            for(int i = inx; i < prices.length; i++) {
-                cnt++;
-                if(poll > prices[i]) {
-                    break;
-                }
-            }
-            answer[inx] = cnt;
-            inx++;
+
+        while(!stack.isEmpty()) {
+            answer[stack.peek()] = prices.length - stack.pop() -1;
         }
         return answer;
     }
