9793321da164bf03d678ff7fb7eb3aef6c548683
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-22 16:16:47 +0900

    [level 2] Title: H-Index, Time: 2.47 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
 class Solution {
     public int solution(int[] citations) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i : citations) {
-            list.add(i);
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int num : citations) {
+            arr.add(num);
         }
-        list.sort((a,b) -> Integer.compare(b,a));
+
+        arr.sort((a,b) -> Integer.compare(b,a));
         int answer = 0;
-        for(int i = 0; i < list.size(); i++) {
-            if(list.get(i) >= i+1) {
-                answer = i+1;
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr.get(i) >= i + 1) {
+                answer = i + 1;
             } else {
                 break;
             }
         }
+
         return answer;
     }
 }
