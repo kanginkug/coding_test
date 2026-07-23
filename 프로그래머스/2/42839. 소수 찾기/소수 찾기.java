6771d260610d86552b46ebf156b8490773b90b38
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-23 16:29:03 +0900

    [level 2] Title: 소수 찾기, Time: 52.31 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,24 +1,28 @@
 import java.util.HashSet;
 class Solution {
-    HashSet<Integer> numSet = new HashSet<>();
-    int answer = 0;
-    // 19시 51분
+    HashSet<Integer> hashSet = new HashSet<>();
     public int solution(String numbers) {
-
-
-        recursive("", numbers);
-        int answer = Prime(numSet);
-        System.out.println(numSet.toString());
+        int answer = 0;
+        Recursive("", numbers);
+        answer = Prime(hashSet);
         return answer;
     }
+    public void Recursive(String comp, String other) {
+        if(!comp.isEmpty()) {
+            hashSet.add(Integer.parseInt(comp));
+        }
+        for(int i = 0; i < other.length(); i++) {
+            Recursive(comp + other.charAt(i), other.substring(0,i) + other.substring(i+1));
+        }
+    }
 
-    public int Prime(HashSet<Integer> numSet) {
-        for(int num : numSet) {
+    public int Prime(HashSet<Integer> hashSet) {
+        int answer = 0;
+        for(int num : hashSet) {
             if(num != 1 && num != 0) {
                 boolean isPrime = true;
-                int a = num/2+1;
-                for(int i = 2; i < a; i++) {
-                    if(num%i == 0) {
+                for(int i = 2; i < (num/2+1); i++) {
+                    if(num%i==0) {
                         isPrime = false;
                         break;
                     }
@@ -29,14 +33,5 @@ class Solution {
             }
         }
         return answer;
-    }
-
-    public void recursive(String a, String b) {
-        if(!a.isEmpty()) {
-            numSet.add(Integer.parseInt(a));
-        }
-        for(int i = 0; i < b.length(); i++) {
-            recursive(a + b.charAt(i), b.substring(0,i) + b.substring(i+1));
-        }
     }
 }
