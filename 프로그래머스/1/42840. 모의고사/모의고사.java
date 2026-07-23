c3eaaf9d4a518b7a33d32d155fc52bd59f523e7d
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-23 16:10:42 +0900

    [level 1] Title: 모의고사, Time: 0.48 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -5,26 +5,28 @@ class Solution {
         int[] person1 = {1, 2, 3, 4, 5};
         int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] nums = new int[3];
+        int[] answerArr = {0,0,0};
         for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == person1[i%person1.length]) {
-                nums[0] = nums[0]+1;
+            if(answers[i] == person1[i % person1.length]) {
+                answerArr[0] += 1;
             }
-            if(answers[i] == person2[i%person2.length]) {
-                nums[1] = nums[1]+1;
+            if(answers[i] == person2[i % person2.length]) {
+                answerArr[1] += 1;
             }
-            if(answers[i] == person3[i%person3.length]) {
-                nums[2] = nums[2]+1;
+            if(answers[i] == person3[i % person3.length]) {
+                answerArr[2] += 1;
             }
         }
-        int max = Math.max(nums[0], Math.max(nums[1],nums[2]));
-        ArrayList<Integer> arr = new ArrayList<>();
-        if(nums[0] == max) arr.add(1);
-        if(nums[1] == max) arr.add(2);
-        if(nums[2] == max) arr.add(3);
-        int[] answer = new int[arr.size()];
-        for(int i = 0; i < arr.size(); i++) {
-            answer[i] = arr.get(i);
+        int max = Math.max(answerArr[0], Math.max(answerArr[1], answerArr[2]));
+
+        ArrayList<Integer> arrList = new ArrayList<>();
+        if(answerArr[0] == max) arrList.add(1);
+        if(answerArr[1] == max) arrList.add(2);
+        if(answerArr[2] == max) arrList.add(3);
+
+        int[] answer = new int[arrList.size()];
+        for(int i = 0; i < arrList.size(); i++) {
+            answer[i] = arrList.get(i);
         }
         return answer;
     }
