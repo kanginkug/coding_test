483d01d65c83bcd841b63ed8fe243c344b2858f7
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-24 15:17:39 +0900

    [level 2] Title: 방문 길이, Time: 11.05 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,37 +1,42 @@
 import java.util.HashSet;
 class Solution {
+    HashSet<String> hashSet = new HashSet<>();
     public int solution(String dirs) {
-        HashSet<String> hashSet = new HashSet<>();
         int[] standard = {0,0};
         for(int i = 0; i < dirs.length(); i++) {
-            String beforeStand = "";
-            String afterStand = "";
-            if(dirs.charAt(i) == 'U' && standard[1]+1 <= 5) {
-                beforeStand = String.valueOf(standard[0]) + standard[1];
-                standard[1] += 1;
-                afterStand = String.valueOf(standard[0]) + standard[1];
-                hashSet.add(beforeStand + "," + afterStand);
-                hashSet.add(afterStand + "," + beforeStand);
-            } else if(dirs.charAt(i) == 'D' && standard[1]-1 >= -5) {
-                beforeStand = String.valueOf(standard[0]) + standard[1];
-                standard[1] -= 1;
-                afterStand = String.valueOf(standard[0]) + standard[1];
-                hashSet.add(beforeStand + "," + afterStand);
-                hashSet.add(afterStand + "," + beforeStand);
-            } else if(dirs.charAt(i) == 'R' && standard[0]+1 <= 5) {
-                beforeStand = String.valueOf(standard[0]) + standard[1];
-                standard[0] += 1;
-                afterStand = String.valueOf(standard[0]) + standard[1];
-                hashSet.add(beforeStand + "," + afterStand);
-                hashSet.add(afterStand + "," + beforeStand);
-            }else if(dirs.charAt(i) == 'L' && standard[0]-1 >= -5) {
-                beforeStand = String.valueOf(standard[0]) + standard[1];
-                standard[0] -= 1;
-                afterStand = String.valueOf(standard[0]) + standard[1];
-                hashSet.add(beforeStand + "," + afterStand);
-                hashSet.add(afterStand + "," + beforeStand);
+
+            int[] before = new int[2];
+            int[] after = new int[2];
+            if(dirs.charAt(i) == 'U' && standard[1] + 1 <= 5) {
+                before = new int[]{standard[0], standard[1]};
+                standard[1]+= 1;
+                after = new int[]{standard[0], standard[1]};
+                addHashSet(before, after);
+            } else if(dirs.charAt(i) == 'D' && standard[1] -1  >= -5) {
+                before = new int[]{standard[0], standard[1]};
+                standard[1]-= 1;
+                after = new int[]{standard[0], standard[1]};
+                addHashSet(before, after);
+            } else if(dirs.charAt(i) == 'R'&& standard[0] + 1 <= 5) {
+                before = new int[]{standard[0], standard[1]};
+                standard[0]+= 1;
+                after = new int[]{standard[0], standard[1]};
+                addHashSet(before, after);
+            } else if(dirs.charAt(i) == 'L' && standard[0] -1  >= -5) {
+                before = new int[]{standard[0], standard[1]};
+                standard[0]-= 1;
+                after = new int[]{standard[0], standard[1]};
+                addHashSet(before, after);
             }
         }
         return hashSet.size()/2;
+    }
+    public void addHashSet(int[] before, int[] after) {
+        String hashStr = "";
+        String hashStr2 = "";
+        hashStr2 = String.valueOf(before[0]) + "," + String.valueOf(before[1]) + "->" + String.valueOf(after[0]) + "," + String.valueOf(after[1]);
+        hashStr = String.valueOf(after[0]) + "," + String.valueOf(after[1]) + "->" + String.valueOf(before[0]) + "," + String.valueOf(before[1]);
+        hashSet.add(hashStr);
+        hashSet.add(hashStr2);
     }
 }
