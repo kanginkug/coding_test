1705d2b58d92ab0fd24b3a4222e205e1d039242f
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-07-28 16:34:57 +0900

    [level 2] Title: 타겟 넘버, Time: 6.04 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,19 @@
 class Solution {
-    int answer = 0;
+    int cnt = 0;
     public int solution(int[] numbers, int target) {
-        // index = 0번째 숫자부터 시작, 현재 sum = 0
-        dfs(numbers, target, 0, 0);
-        return answer;
+        int inx = 0;
+        int sum = 0;
+        DFS(inx, numbers, target, sum);
+        return cnt;
     }
-    public void dfs(int[] numbers, int target, int index, int sum) {
-        // 1. [탈출 조건] 모든 숫자를 다 검사했을 때
-        if (index == numbers.length) {
-            // 타겟 넘버를 만들었다면 정답 추가!
-            if (sum == target) {
-                answer++;
-            }
-            return; // ★ 반드시 return으로 함수를 끝내줘야 함!
+    public void DFS(int inx, int[] numbers, int target, int sum) {
+        if(inx == numbers.length && sum == target) {
+            cnt ++;
         }
-
-        // 2. [두 갈림길 선택]
-        // 현재 숫자(numbers[index])를 빼는 경우 (-)
-        dfs(numbers, target, index + 1, sum - numbers[index]);
-
-        // 현재 숫자(numbers[index])를 더하는 경우 (+)
-        dfs(numbers, target, index + 1, sum + numbers[index]);
+        if(inx == numbers.length) {
+            return;
+        }
+        DFS(inx+1, numbers, target, sum - numbers[inx]);
+        DFS(inx+1, numbers, target, sum + numbers[inx]);
     }
 }
