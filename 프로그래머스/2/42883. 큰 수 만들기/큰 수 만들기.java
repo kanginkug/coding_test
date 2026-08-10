edea71ae8a815045d754af3b23225aac5623584b
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-08-10 12:47:43 +0900

    [level 2] Title: 큰 수 만들기, Time: 729.26 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,32 +1,24 @@
-import java.util.Stack;
+import java.util.ArrayDeque;
 class Solution {
     public String solution(String number, int k) {
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < number.length(); i++) {
-            char c = number.charAt(i);
-
-            // 새로 들어올 숫자 c가 스택 상단 숫자보다 크면 계속 pop
-            while (!stack.isEmpty() && k > 0 && stack.peek() < c) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < number.length(); i++) {
+            while (k>0 && !stack.isEmpty() && stack.peek() < number.charAt(i)) {
                 stack.pop();
                 k--;
             }
-
-            stack.push(c);
+            stack.push(number.charAt(i));
+        }
+        while(k > 0) {
+            
+                    stack.pop();
+                    k--;
+        }
+        while(!stack.isEmpty()) {
+            answer.insert(0, stack.pop());
         }
 
-        // 만약 "9876", k=2 처럼 이미 내림차순이라 k가 남았다면 뒤에서부터 k개 제거
-        while (k > 0) {
-            stack.pop();
-            k--;
-        }
-
-        // Stack의 모든 문자를 하나로 합치기
-        char[] result = new char[stack.size()];
-        for (int i = result.length - 1; i >= 0; i--) {
-            result[i] = stack.pop();
-        }
-
-        return new String(result);
+        return answer.toString();
     }
 }
