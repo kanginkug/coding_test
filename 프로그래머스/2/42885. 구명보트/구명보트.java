c39f7c72dd05e7fb8968d97b1fc34921330ccdee
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-08-10 12:06:09 +0900

    [level 2] Title: 구명보트, Time: 10.41 ms, Memory: 59.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int right = people.length-1;
+        int left = 0;
+        int answer = 0;
+        while(left<=right) {
+            if(people[right] + people[left] <= limit) {
+                left++;
+            }
+            right--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
