1c2b6ace497570915acd0ec3aa7e0ec9a725fead
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-08-10 17:48:02 +0900

    [level 2] Title: 큰 수 만들기, Time: 4498.21 ms, Memory: 393 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,23 +2,24 @@ import java.util.ArrayDeque;
 class Solution {
     public String solution(String number, int k) {
         ArrayDeque<Character> stack = new ArrayDeque<>();
-        StringBuilder answer = new StringBuilder();
-        for(int i = 0; i < number.length(); i++) {
-            while (k>0 && !stack.isEmpty() && stack.peek() < number.charAt(i)) {
-                stack.pop();
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i =0; i < number.length(); i++) {
+            while(!stack.isEmpty() && k > 0 && stack.peek() < number.charAt(i)) {
                 k--;
+                stack.pop();
             }
             stack.push(number.charAt(i));
         }
-        while(k > 0) {
-            
-                    stack.pop();
-                    k--;
+        if(k > 0) {
+            for(int i = 0; i < k; i++) {
+                stack.pop();
+            }
         }
         while(!stack.isEmpty()) {
-            answer.insert(0, stack.pop());
+            answer = stack.pop() + answer;
         }
 
-        return answer.toString();
+        return answer;
     }
 }
