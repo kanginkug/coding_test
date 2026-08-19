49eb77b2f68c08032089a3cb540b41593d09f8ce
kanginkug <83181058+kanginkug@users.noreply.github.com>
2026-08-19 14:45:23 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 2.09 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,59 +1,45 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 class Solution {
     public int solution(String str1, String str2) {
-        ArrayList<String> list1 = new ArrayList<>();
-        ArrayList<String> list2 = new ArrayList<>();
-
+        HashMap<String, Integer> str1Map = new HashMap<>();
+        HashMap<String, Integer> str2Map = new HashMap<>();
         str1 = str1.toUpperCase();
         str2 = str2.toUpperCase();
-
-        // 1. str1 다중집합 생성 (순수 영문자 쌍만 추가)
-        for (int i = 0; i < str1.length() - 1; i++) {
-            char a = str1.charAt(i);
-            char b = str1.charAt(i + 1);
-
-            if (isAlpha(a) && isAlpha(b)) {
-                list1.add("" + a + b);
-            }
-        }
-
-        // 2. str2 다중집합 생성 (순수 영문자 쌍만 추가)
-        for (int i = 0; i < str2.length() - 1; i++) {
-            char a = str2.charAt(i);
-            char b = str2.charAt(i + 1);
-
-            if (isAlpha(a) && isAlpha(b)) {
-                list2.add("" + a + b);
-            }
-        }
-
-        // 3. 두 집합 모두 공집합인 경우 예외 처리
-        if (list1.isEmpty() && list2.isEmpty()) {
+        str1Map = tranceMap(str1, str1Map);
+        str2Map = tranceMap(str2, str2Map);
+        if(str1Map.isEmpty() && str2Map.isEmpty()) {
             return 65536;
         }
 
-        // 4. 교집합 및 합집합 계산
-        ArrayList<String> intersection = new ArrayList<>();
-        ArrayList<String> union = new ArrayList<>();
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.addAll(str1Map.keySet());
+        hashSet.addAll(str2Map.keySet());
 
-        for (String s : list1) {
-            // list2에 s가 포함되어 있다면 교집합에 추가 후 list2에서 1개 제거
-            if (list2.remove(s)) {
-                intersection.add(s);
+        int min = 0;
+        int max = 0;
+        for(String key : hashSet) {
+            if(str1Map.get(key) != null && str2Map.get(key) != null) {
+                min += Math.min(str1Map.get(key), str2Map.get(key));
+                max += Math.max(str1Map.get(key), str2Map.get(key));
+            } else if(str1Map.get(key) == null) {
+                max += str2Map.get(key);
+            } else if(str2Map.get(key) == null) {
+                max += str1Map.get(key);
             }
-            union.add(s);
         }
-
-        // list2에 남아있는 원소들을 합집합에 마저 추가
-        union.addAll(list2);
-
-        // 5. 자카드 유사도 계산 후 65536 곱하기
-        double similarity = (double) intersection.size() / union.size();
-        return (int) (similarity * 65536);
+        double share = (double) min /max;
+        return (int) (share*65536);
     }
-
-    // 영문 대문자인지 확인하는 헬퍼 메서드
-    private boolean isAlpha(char ch) {
-        return ch >= 'A' && ch <= 'Z';
+    public HashMap<String, Integer> tranceMap(String str, HashMap<String, Integer> strMap) {
+        for(int i = 0; i < str.length(); i++) {
+            if(i+1 < str.length()) {
+                if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z' && str.charAt(i+1) >= 'A' && str.charAt(i+1) <= 'Z') {
+                    String sumStr = String.valueOf(str.charAt(i)) + String.valueOf(str.charAt(i+1));
+                    strMap.put(sumStr, strMap.getOrDefault(sumStr,0)+1);
+                }
+            }
+        }
+        return strMap;
     }
 }
